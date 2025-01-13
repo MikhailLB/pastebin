436b3d7d6962b78aad1cad0d8d91ebb7a0ca4332
Mikhail <lobodamisha29@gmail.com>
2025-01-13 15:41:20 +0100

    Update views.py

--- pastebin/api/views.py
+++ pastebin/api/views.py
@@ -12,23 +12,15 @@ class Main(FormView):
     template_name = 'api/input.html'
 
     def get(self, request, *args, **kwargs):
-        # Очистка сессии при загрузке страницы
         request.session['links'] = []
         return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
-        # Сохранение объекта, включая генерацию уникального хэша
         paste = form.save()
-
-        # Генерация уникальной ссылки
         unique_link = self.request.build_absolute_uri(f'/{paste.unique_hash}/')
-
-        # Добавляем ссылку в сессию
         links = self.request.session.get('links', [])
         links.append(unique_link)
         self.request.session['links'] = links
-
-        # Отображение страницы с обновленным списком ссылок
         return self.render_to_response(self.get_context_data(links=links))
 
 
@@ -37,27 +29,22 @@ from .models import PasteBin
 
 
 def check_text(request, unique_hash):
-    # Извлекаем объект PasteBin по уникальному хэшу или возвращаем 404, если не найден
     text_object = get_object_or_404(PasteBin, unique_hash=unique_hash)
-
-    # Передаем текст и уникальный хэш в шаблон
     return render(request, 'api/user_text.html', {
-        'text': text_object.content,  # текст, который нужно отобразить
-        'unique_hash': unique_hash  # уникальный хэш для отображения ссылки
+        'text': text_object.content,
+        'unique_hash': unique_hash 
     })
 
 
 class UpdateText(UpdateView):
     model = PasteBin
     fields = ['content']
-    template_name = 'api/user_update.html'  # Шаблон для редактирования текста
+    template_name = 'api/user_update.html'
     context_object_name = 'pastebin'
 
     def get_object(self, queryset=None):
-        # Ищем объект по уникальному хэшу
         unique_hash = self.kwargs.get('unique_hash')
         return get_object_or_404(PasteBin, unique_hash=unique_hash)
 
     def get_success_url(self):
-        # После успешного редактирования перенаправляем пользователя обратно на страницу просмотра текста
         return reverse_lazy('text', kwargs={'unique_hash': self.object.unique_hash})
