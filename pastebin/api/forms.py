3b2fd1318906e9d308e30284834ff2896e906d69
Mikhail <lobodamisha29@gmail.com>
2025-01-13 15:41:48 +0100

    Update forms.py

--- pastebin/api/forms.py
+++ pastebin/api/forms.py
@@ -8,11 +8,11 @@ class TextInputForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'cols': 50, 'rows': 5})
         }
         labels = {
-            'content': '',  # Убираем label
+            'content': '',
         }
     def clean_text(self):
         text = self.cleaned_data.get('content')
-        max_size = 10 * 1024 * 1024  # 10 МБ в байтах
+        max_size = 10 * 1024 * 1024 
 
         if len(text.encode('utf-8')) > max_size:
             raise forms.ValidationError("Текст превышает допустимый размер 10 МБ.")
